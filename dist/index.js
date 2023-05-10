24e354ae96c509c42f0d863d110014fd2f0f3648
tomit4 <mosssap@gmail.com>
2023-05-10 03:44:16 -0700

    :lipstick: Added interactive darkmode

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,22 @@
-const toggleLight = () => {
-    console.log('toggleLight() hit')
+const icons = document.querySelectorAll('.icons')
+const temp = document.querySelectorAll('.temp')
+
+window.onload = () => {
+    if (!localStorage['data-theme'])
+        window.matchMedia('(prefers-color-scheme: light)').matches ? 
+            toggleTemp('light') : 
+            toggleTemp('dark')
+    else
+        localStorage['data-theme'] === 'light' ? 
+            toggleTemp('light') :
+            toggleTemp('dark')
 }
 
-const toggleDark = () => {
-    console.log('toggleDark() hit')
+const toggleTemp = (theme) => {
+    icons.forEach(icon => icon.setAttribute('data-theme', `${theme}`))
+    document.documentElement.setAttribute('data-theme', `${theme}`)
+    localStorage.setItem('data-theme', `${theme}`)
 }
+
+temp.forEach(t => t.addEventListener('click', () => 
+    t.id === 'sun' ? toggleTemp('light') : toggleTemp('dark')))
