c7d2cf85215f71629d4fc4a02c1b8e3f82d9be9f
tomit4 <mosssap@gmail.com>
2023-05-12 22:16:22 -0700

    :sparkles: Added text hover effect to nav and foot

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,34 @@
 const icons = document.querySelectorAll('.icons')
 const temp = document.querySelectorAll('.temp')
+const navTextItems = document.querySelectorAll('.nav-text-item')
+const footTextItems = document.querySelectorAll('.foot-text-item')
+
+// TODO: consider a more eloquent approach, no double for loops...
+// create an array of just the ids of each array and then compare them, 
+// use one for loop to compare and log the index to do this(?)
+icons.forEach(icon => {
+    icon.addEventListener('mouseover', () => {
+        navTextItems.forEach(navText => {
+            if (`text-${icon.id}` === navText.id)
+                navText.style.visibility = 'visible'
+        })
+        footTextItems.forEach(footText => {
+            if (`text-${icon.id}` === footText.id)
+                footText.style.visibility = 'visible'
+        })
+
+    })
+    icon.addEventListener('mouseleave', () => {
+        navTextItems.forEach(navText => {
+            if (`text-${icon.id}` === navText.id)
+                navText.style.visibility = 'hidden'
+        })
+        footTextItems.forEach(footText => {
+            if (`text-${icon.id}` === footText.id)
+                 footText.style.visibility = 'hidden'
+        })
+    })
+})
 
 window.onload = () => {
     if (!localStorage['data-theme'])
