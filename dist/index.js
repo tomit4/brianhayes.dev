f471b366bb2e4d24efa98bd71f26744249225618
tomit4 <mosssap@gmail.com>
2023-06-02 02:31:24 -0700

    :sparkles: Added darkreader toggle

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ const arrowTextItems = document.querySelectorAll('.arrow-text')
 const footerButtons = document.querySelectorAll('.footer-button')
 
 // TODO: consider a more eloquent approach, no double for loops...
-// create an array of just the ids of each array and then compare them, 
+// create an array of just the ids of each array and then compare them,
 // use one for loop to compare and log the index to do this(?)
 icons.forEach(icon => {
     icon.addEventListener('mouseover', () => {
@@ -21,7 +21,6 @@ icons.forEach(icon => {
             if (`text-${icon.id}` === arrowText.id)
                 arrowText.style.visibility = 'visible'
         })
-
     })
     icon.addEventListener('mouseleave', () => {
         navTextItems.forEach(navText => {
@@ -30,7 +29,7 @@ icons.forEach(icon => {
         })
         footTextItems.forEach(footText => {
             if (`text-${icon.id}` === footText.id)
-                 footText.style.visibility = 'hidden'
+                footText.style.visibility = 'hidden'
         })
         arrowTextItems.forEach(arrowText => {
             if (`text-${icon.id}` === arrowText.id)
@@ -41,24 +40,32 @@ icons.forEach(icon => {
 
 footerButtons.forEach(button =>
     button.addEventListener('click', () =>
-        button.id === 'sun-button' ?
-        toggleTemp('light') :
-        toggleTemp('dark'))
+        button.id === 'sun-button' ? toggleTemp('light') : toggleTemp('dark'),
+    ),
 )
 
 window.onload = () => {
     if (!localStorage['data-theme'])
-        window.matchMedia('(prefers-color-scheme: light)').matches ?
-            toggleTemp('light') :
-            toggleTemp('dark')
+        window.matchMedia('(prefers-color-scheme: light)').matches
+            ? toggleTemp('light')
+            : toggleTemp('dark')
     else
-        localStorage['data-theme'] === 'light' ?
-            toggleTemp('light') :
-            toggleTemp('dark')
+        localStorage['data-theme'] === 'light'
+            ? toggleTemp('light')
+            : toggleTemp('dark')
 }
 
-const toggleTemp = (theme) => {
+const toggleTemp = theme => {
     icons.forEach(icon => icon.setAttribute('data-theme', `${theme}`))
     document.documentElement.setAttribute('data-theme', `${theme}`)
     localStorage.setItem('data-theme', `${theme}`)
 }
+
+// Dark Reader Adjustments, logic taken from:
+// https://github.com/darkreader/darkreader/issues/4342
+new MutationObserver(() => {
+    const isDarkReaderEnabled =
+        'querySelector' in document &&
+        !!document.querySelector('meta[name=darkreader]')
+    isDarkReaderEnabled ? toggleTemp('dark') : toggleTemp('light')
+}).observe(document.querySelector('head'), { childList: true })
